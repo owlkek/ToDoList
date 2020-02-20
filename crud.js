5e64b056fb469173b7996b84da1e607eeaa17448
owlkek <46930203+owlkek@users.noreply.github.com>
2020-02-21 00:06:22 +0300

    Add files via upload

---
+++ crud.js
@@ -0,0 +1,45 @@
+const {MainViewModel} = require("../models");
+let db = require("../utils/sqlitedb");
+
+exports.get = (r, q) => {
+    db.getTasks(+r.params.id).then(item => {
+        if(item) {
+            console.log(item);
+            db.getTasks().then(tasks => {
+                db.getStatuses().then(statuses => {
+                    let model = new MainViewModel("TODO LIST", tasks, statuses, item);
+                    q.render("index", model);
+                });
+            });
+        }
+        else {
+            q.redirect("/");
+        }
+    });
+}
+
+exports.add = (r, q) => {
+        
+    db.getStatuses(+r.body.status).then(status => {
+        r.body.status = status;
+        db.addTask(r.body).then(x => {
+            q.redirect("/");
+        });
+    });
+}
+
+exports.update = (r, q) => {
+    r.body.id = +r.body.id;
+    db.getStatuses(+r.body.status).then(x => {
+        r.body.status = x;
+        db.updateTask(r.body).then(y => {
+            q.redirect("/");
+        });
+    });
+}
+
+exports.delete = (r, q) => {
+    db.removeTask(+r.params.id).then(x => {
+        q.redirect("/");
+    });
+}
---
+++ routes/main.js
@@ -0,0 +1,11 @@
+const {MainViewModel} = require('../models');
+const db = require('../utils/sqlitedb');
+
+module.exports = (r, q) => {
+    db.getTasks().then(task =>{
+        db.getStatuses().then(statuses =>{
+            let model = new MainViewModel('TODO LIST', task, statuses);
+            q.render('index', model);
+        });
+    });
+}
