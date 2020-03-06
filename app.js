1d5ec80fe119a964fb4d80a7d5d2fe6e1e93f876
owlkek <46930203+owlkek@users.noreply.github.com>
2020-03-06 10:32:19 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
-const errorHandler = require('./routes/404');
-let port = 8080;
+const http = require('http');
+let port = process.env.PORT || 3000;
 let app = express();
 app.set('view engine','pug');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.use('/',routes);
-app.use(errorHandler);
-
 app.listen(port, ()=>{
     console.info(`App run on http://localhost:${port}`);
 });
